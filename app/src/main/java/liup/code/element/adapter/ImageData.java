a961edcad877482f70f50f8d18dc428df82621c2
XiaoLiu <840387149@qq.com>
2018-03-01 11:05:26 +0800

    项目初始化

---
+++ app/src/main/java/liup/code/element/adapter/ImageData.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package liup.code.element.adapter;
+
+import android.support.annotation.DrawableRes;
+
+import liup.code.element.R;
+
+/**
+ * Holds the image resource references used by the grid and the pager fragments.
+ */
+abstract class ImageData {
+
+  // Image assets (free for commercial use, no attribution required, from pixabay.com)
+  @DrawableRes
+  static final int[] IMAGE_DRAWABLES = {
+          R.drawable.cfdbc2efef87499a9cba304452c64a27,
+          R.drawable.image1,
+          R.drawable.image2,
+          R.drawable.image3,
+          R.drawable.wallhaven281671,
+          R.drawable.spoon2426623,
+          R.drawable.wallhaven62159,
+          R.drawable.wallhaven75060,
+          R.drawable.wallhaven398613,
+          R.drawable.wallhaven433199,
+          R.drawable.wallhaven140200,
+          R.drawable.wallhaven497378,
+          R.drawable.wallhaven178923,
+          R.drawable.wallhaven185927
+  };
+
+}
